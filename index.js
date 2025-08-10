01d63b84ac5ae797d6fcbddbc655d4b029f3f3e1
Itachi-Uchiha-32 <abid3082002@gmail.com>
2025-08-10 19:59:36 +0600

    fifth

--- index.js
+++ index.js
@@ -48,6 +48,7 @@ const client = new MongoClient(uri, {
 // Middleware to verify Firebase ID token
 const verifyFirebaseToken = async (req, res, next) => {
   const authHeader = req.headers?.authorization;
+  console.log(authHeader)
   if (!authHeader?.startsWith('Bearer ')) {
     return res.status(401).send({ message: 'unauthorized access' });
   }
